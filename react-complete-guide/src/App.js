5d2e66f99321d9dcbff234563c4d7964ac712d32
HrithikMittal <adhikanshmittal@gmail.com>
2019-07-21 22:20:41 +0530

    readme.md

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,39 +5,44 @@ import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    persons: [
-      {
-        name: "Adhikansh",
-        age: 28
-      },
-      {
-        name: "Vidushi",
-        age: 20
-      },
-      {
-        name: "Harshita",
-        age: 20
-      }
-    ]
+    person: []
+  };
+  switchNameHandler = () => {
+    this.setState = {
+      persons: [
+        {
+          name: "Adhikansh",
+          age: 28
+        },
+        {
+          name: "Vidushi",
+          age: 20
+        },
+        {
+          name: "Harshita",
+          age: 20
+        }
+      ]
+    };
   };
 
   render() {
     return (
       <div>
         <h1>Hello I'm a React App.</h1>
-        <button>Swicth Name</button>
-        <Person
+        <button onClick={this.switchNameHandler()}>Swicth Name</button>
+        {/* <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
-        />
-        <Person
+        /> */}
+        {/* <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
         />
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
-        />
+        /> */}
       </div>
     );
     // return React.createElement(
