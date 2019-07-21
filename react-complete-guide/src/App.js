ce5e95a4abbae3a2b601e2fbe273723fc9d9a7d5
HrithikMittal <adhikanshmittal@gmail.com>
2019-07-21 20:51:08 +0530

    backside of JSX

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello I am Adhikansh</h1>
+      </div>
+    );
+    // return React.createElement(
+    //   "div",
+    //   null,
+    //   React.createElement("h1", null, "Hi, I'm a React App!!!")
+    // );
+  }
+}
+
+export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Person = () => {
+  return (
+    <div>
+      <h1>Hi I'm Adhikansh</h1>
+    </div>
+  );
+};
+
+export default Person;
