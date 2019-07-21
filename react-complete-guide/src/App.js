9fd5fe91460b8fc9f221dbe12886c8397d951a84
HrithikMittal <adhikanshmittal@gmail.com>
2019-07-21 20:52:11 +0530

    adding 1st component of react

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Person from "./Person/Person";
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello I am Adhikansh</h1>
+        <Person />
       </div>
     );
     // return React.createElement(
