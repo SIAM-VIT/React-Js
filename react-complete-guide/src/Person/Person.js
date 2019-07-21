86bf7b93a89187dc7801df09b9f947cd937912f5
HrithikMittal <adhikanshmittal@gmail.com>
2019-07-21 21:03:41 +0530

    run js into JSX

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from "react";
 const Person = () => {
   return (
     <div>
-      <h1>Hi I'm Adhikansh</h1>
+      <h1>Hi I'm Person and I am {Math.floor(Math.random() * 30)} year old</h1>
     </div>
   );
 };
