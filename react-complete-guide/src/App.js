b16a5058eb91ead9c56ed121756b3b4ae9be355d
HrithikMittal <adhikanshmittal@gmail.com>
2019-07-21 21:32:32 +0530

    look on state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,40 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Adhikansh",
+        age: 28
+      },
+      {
+        name: "Vidushi",
+        age: 20
+      },
+      {
+        name: "Harshita",
+        age: 20
+      }
+    ]
+  };
+
   render() {
     return (
       <div>
         <h1>Hello I'm a React App.</h1>
-        <Person name="Adhikansh" age="18" />
-        <Person name="Vidushi" age="20">
-          My Hobbies:Drawing
-        </Person>
+        <button>Swicth Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement(
