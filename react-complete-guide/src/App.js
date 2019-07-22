5bc0cef53542b3dd839658b433b01ed8b192a65c
HrithikMittal <adhikanshmittal@gmail.com>
2019-07-22 12:23:10 +0530

    dyanically classes for css

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -106,7 +106,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -145,11 +146,21 @@ class App extends Component {
           /> */}
         </div>
       );
+      style.backgroundColor = "red";
+    }
+
+    const classes = [];
+    if (this.state.persons.length < 2) {
+      classes.push("red"); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold"); //classes = ['red','bold']
     }
 
     return (
-      <div>
+      <div className="App">
         <h1>Hello I'm a React App.</h1>
+        <p className={classes.join(" ")}>This is really working!</p>
         <button
           style={style}
           // onClick={this.switchNameHandler.bind(this, "Hrihtik")}
